036b3231ef982a8719bf31638a767c3b95d74bbe
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-12-03 18:00:24 +0100

    Add csv data and build APIs usin taffy

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var express = require('express');
+var TAFFY = require("taffy");
 var Botkit = require('botkit');
 var _ = require('underscore');
 var controller = Botkit.slackbot();
@@ -26,6 +27,10 @@ function matcher(text) {
 
 var requestConfig = [
     {
+        'pattern': ['Info', 'fakten'],
+        'answerFn': answers.info
+    },
+    {
         'pattern': ['hi', 'hello', 'hey'],
         'answerFn': answers.hello
     }
@@ -48,8 +53,71 @@ controller.hears(['.*'], ['direct_message,direct_mention'], function(bot, messag
 });
 
 
-// To keep Heroku's free dyno awake
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Ok, dyno is awake.');
-}).listen(process.env.PORT || 5000);
+// the address API
+var app = express();
+
+// Enable CORS so that we can actually use the server for something useful.
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+var Converter = require("csvtojson").Converter;
+var csvConverter = new Converter({'delimiter': ';'});
+
+var addresses = null;
+csvConverter.fromFile('./data/adressen.csv', function(err, jsonObj) {
+    console.log("Error: ", err);
+    addresses = TAFFY(jsonObj);
+    console.log(addresses().get());
+    console.log("Finished parsing adresses CSV");
+});
+
+var wbev = null;
+var wbevConverter = new Converter({delimiter: ','});
+wbevConverter.fromFile('./data/bevbestandjahradminsherkunftreligion.csv', function(err, jsonObj) {
+    console.log("Error: ", err);
+    wbev = TAFFY(jsonObj);
+    console.log(wbev().get());
+    console.log("Finished parsing wbev CSV");
+});
+
+
+app.get('/address/:address?', function(req, res) {
+    try {
+        var result = {"result": addresses().get() };
+        console.log("req.params.address:", req.params.address);
+        if (req.params.address) {
+    	    result.result = addresses({'Adresse': {likenocase: req.params.address}}).get();
+        } 
+    	res.json(result);
+    } catch (ex) {
+        res.status(500).send("The query could not be executed.");
+    }
+});
+
+app.get('/wbev/:statzone?', function(req, res) {
+    try {
+        var result = wbev();
+        console.log("req.params.statzone:", req.params.statzone);
+        console.log("req.query.year:", req.query.year);
+        if (req.params.statzone) {
+    	    result = wbev({'StatZoneSort': {"==": req.params.statzone}});
+        } 
+        if (req.query.year) {
+            result = result.filter({"StichtagDatJahr": {"==": req.query.year}});
+        }
+    	res.json({"result": result.get() });
+    } catch (ex) {
+        res.status(500).send("The query could not be executed.");
+    }
+});
+
+app.get('/', function(req, res) {
+	res.status(200).send("The dyno is awake");
+});
+
+var server = app.listen(process.env.PORT || 5000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
