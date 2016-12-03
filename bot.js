e1607e992edf49c7495d7a656cac45c5b3bb1179
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-12-03 10:24:18 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var Botkit = require('botkit');
+var _ = require('underscore');
+var controller = Botkit.slackbot();
+var answers = require('./lib/answers');
+
+var tokens = process.env.token.split(" ");
+_.each(tokens, function(token) {
+    var betty = controller.spawn({
+      token: token
+    });
+    betty.startRTM(function(err,bot,payload) {
+      if (err) {
+        console.log(err);
+        throw new Error('Could not connect to Slack');
+      }
+    });
+});
+
+function matcher(text) {
+    return function(pattern) {
+        var re = new RegExp(pattern, 'i');
+        return re.test(text);
+    };
+}
+
+var requestConfig = [
+    {
+        'pattern': ['hi', 'hello', 'hey'],
+        'answerFn': answers.hello
+    }
+];
+
+
+controller.hears(['.*'], ['direct_message,direct_mention'], function(bot, message) {
+    console.log("Bot heard message (" + message.ts + "): " + message.text);
+    var noAnswer = _.every(requestConfig, function(request) {
+        var matched = _.any(request.pattern, matcher(message.text));
+        if (matched) {
+            request.answerFn(bot, message);
+            return false; //break out of every()
+        }
+        return true; // continue with next requestConfig
+    });
+    if (noAnswer) {
+        answers.didNotUnderstand(bot, message);
+    }
+});
+
+
+// To keep Heroku's free dyno awake
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Ok, dyno is awake.');
+}).listen(process.env.PORT || 5000);
